fa54a93740341c72e44616c2337ff973170308cc
PremanandChowdhuy <freudigprem@gmail.com>
2020-05-18 18:28:22 +0530

    setting up the todoitem

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 
 export default class TodoItem extends Component {
   render() {
+    const { title, onEdit, onDelete } = this.props;
     return (
-      <div>
-        <h4>todo Items</h4>
-      </div>
+      <li className="list-group-item text-capitalize d-flex justify-content-between my-2">
+        <h6>{title}</h6>
+        <div className="todo-icon">
+          <span className="mx-2 text-success" onClick={onEdit}>
+            <i className="fas fa-pen"></i>
+          </span>
+          <span className="mx-2 text-danger" onClick={onDelete}>
+            <i className="fas fa-trash"></i>
+          </span>
+        </div>
+      </li>
     );
   }
 }
